a8c03eda85c5d4918458abd2302341183d73388f
tcnew0 <zhaozebing2012@126.com>
2017-04-17 11:43:47 +0800

    Create MainApp.java

---
+++ interceptor-demo/src/main/java/demo/MainApp.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 15050886 on 2017/4/7.
+ */
+@SpringBootApplication
+public class MainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class,args);
+    }
+}
