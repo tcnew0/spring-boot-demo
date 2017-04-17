02e0c7748dbebf906e7a6a03de0e07c5318b48f3
tcnew0 <zhaozebing2012@126.com>
2017-04-17 11:14:43 +0800

    Create MainApp.java

---
+++ springmvc-demo/src/main/java/demo/MainApp.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * MainApp
+ */
+@SpringBootApplication
+public class MainApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class,args);
+    }
+}
