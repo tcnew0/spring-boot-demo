3afecfde37095b3f74bc95a6e37e154a93479a67
tcnew0 <zhaozebing2012@126.com>
2017-04-17 11:52:18 +0800

    Create SampleWebFreeMarkerApplication.java

---
+++ freemarker-demo/src/main/java/demo/SampleWebFreeMarkerApplication.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 15050886 on 2017/4/6.
+ */
+@SpringBootApplication
+public class SampleWebFreeMarkerApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleWebFreeMarkerApplication.class, args);
+    }
+}
